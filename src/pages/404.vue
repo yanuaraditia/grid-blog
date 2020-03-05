d7ecc8aa803fbf879662841edf3d0c343c76a60e
Yanuar Aditia <yanuaraditia@outlook.com>
2020-03-05 15:35:35 +0700

    Zeroblade

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,21 +1,23 @@
 <template>
-    <section class="text-center">
-        <div class="container-xs">
-            <div class="author_info text-center">
-                <figure>
-                    <g-image src="~/assets/images/sadness.jpg" width="220" height="220" blur="10" alt="Me" class="author_image_mask"/>
-                    <div class="canvas">
-                        <g-image src="~/assets/images/sadness.jpg" width="220" height="220" blur="10" alt="Me" class="author_image"/>
-                    </div>
-                </figure>
-                <div class="info_section">
-                    <h1>404 Not Found</h1>
-                    <h6 class="mb-3 mb-lg-4">Uups....</h6>
-                    <div class="link">
-                        <a class="btn btn-primary" href="//t.me/yanuaraditia">Back Home</a>
+    <Layout :show-logo="false">
+        <section class="text-center">
+            <div class="container-xs">
+                <div class="author_info text-center">
+                    <figure>
+                        <g-image src="~/assets/images/sadness.jpg" width="220" height="220" blur="10" alt="Me" class="author_image_mask"/>
+                        <div class="canvas">
+                            <g-image src="~/assets/images/sadness.jpg" width="220" height="220" blur="10" alt="Me" class="author_image"/>
+                        </div>
+                    </figure>
+                    <div class="info_section">
+                        <h1>404 Not Found</h1>
+                        <h6 class="mb-3 mb-lg-4">Uups....</h6>
+                        <div class="link">
+                            <g-link class="btn btn-primary" to="/">Back Home</g-link>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    </Layout>
 </template>

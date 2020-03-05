e781f44ca5682f3a080b5b8454ab16754914162c
Yanuar Aditia <yanuaraditia@outlook.com>
2020-03-05 15:55:11 +0700

    Zet

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,17 +2,17 @@
   <div id="app">
     <header>
       <b-navbar toggleable="lg" position="sticky" variant="none">
-        <b-navbar-brand to="/" v-if="showLogo">
+        <b-navbar-brand to="/" class="mr-auto" v-if="showLogo">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
         </b-navbar-brand>
-        <b-navbar-toggle target="nav-collapse" class="ml-auto">
+        <ToggleTheme />
+        <b-navbar-toggle target="nav-collapse" class="ml-3">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
         </b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav class="ml-lg-auto">
             <b-nav-item to='/about'>About</b-nav-item>
             <b-nav-item to='/blog'>Blog</b-nav-item>
-            <ToggleTheme />
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // Import main css
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import { BootstrapVue } from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -18,7 +18,6 @@ export default function (Vue, { router, head, isClient }) {
     href: 'https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,400;0,700;1,400&family=Roboto:wght@400;500;700&display=swap'
   })
   Vue.use(BootstrapVue)
-  Vue.use(BootstrapVueIcons)
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }

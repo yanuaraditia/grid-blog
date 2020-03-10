7f3ea13cad7d29495ed7b721a8c1f4cd51cce699
Yanuar Aditia <yanuaraditia@outlook.com>
2020-03-10 15:35:29 +0700

    Note

--- gridsome.config.js
+++ gridsome.config.js
@@ -116,21 +116,3 @@ module.exports = {
     }
   }
 }
-exports.onCreateWebpackConfig = ({
-  stage,
-  actions,
-  getConfig
-}) => {
-  if (stage === 'build-html') {
-    actions.setWebpackConfig({
-      externals: getConfig().externals.concat(function(context, request, callback) {
-        const regex = /^@?firebase(\/(.+))?/;
-        // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
-        if (regex.test(request)) {
-          return callback(null, 'umd ' + request);
-        }
-        callback();
-      })
-    });
-  }
-};
---
+++ src/pages/Contact.vue
@@ -0,0 +1,23 @@
+<template>
+    <Layout :show-logo="false">
+        <section class="text-center">
+            <div class="container-md">
+                <div class="author_info text-center">
+                    <figure>
+                        <g-image src="~/assets/images/sadness.jpg" width="220" height="220" blur="10" alt="Me" class="author_image_mask"/>
+                        <div class="canvas">
+                            <g-image src="~/assets/images/sadness.jpg" width="220" height="220" blur="10" alt="Me" class="author_image"/>
+                        </div>
+                    </figure>
+                    <div class="info_section">
+                        <h1>404 Not Found</h1>
+                        <h6 class="mb-3 mb-lg-4">Uups....</h6>
+                        <div class="link">
+                            <g-link class="btn btn-primary" to="/">Back Home</g-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </Layout>
+</template>

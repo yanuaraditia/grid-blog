eff28e2081960501b7e99e4b7db3284cdf9867cf
Yanuar Aditia <yanuaraditia@outlook.com>
2020-03-10 21:54:52 +0700

    Banting setir

--- src/main.js
+++ src/main.js
@@ -3,12 +3,8 @@ import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '~/assets/style/main.scss'
-
-
-// Import default layout so we don't need to import it to every page
 import DefaultLayout from '~/layouts/Default.vue'
 
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
 export default function (Vue, { router, head, isClient }) {
   
   // Link 
@@ -31,6 +27,5 @@ export default function (Vue, { router, head, isClient }) {
     }
   )
   Vue.use(BootstrapVue)
-  // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -10,7 +10,7 @@
         </figure>
         <div class="info_section">
           <h1>{{ $static.metadata.siteName }}</h1>
-          <h6 class="mb-3 mb-lg-4">Front-End Developer</h6>
+          <h6 class="mb-3 mb-lg-4">UI / UX and Front-End Developer</h6>
           <div class="link">
             <a href="//t.me/yanuaraditia">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-send"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg>

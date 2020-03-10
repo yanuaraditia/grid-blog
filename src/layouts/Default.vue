b5a4aa65df96fd5830b024c6a3ac744ac89afe14
Yanuar Aditia <yanuaraditia@outlook.com>
2020-03-10 15:17:46 +0700

    Rollback

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -9,7 +9,6 @@
           <b-navbar-nav class="ml-lg-auto">
             <b-nav-item to='/'>Home</b-nav-item>
             <b-nav-item to='/about'>About Me</b-nav-item>
-            <b-nav-item to='/contact'>Contact</b-nav-item>
             <b-nav-item to='/blog'>Blog</b-nav-item>
           </b-navbar-nav>
         </b-collapse>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -15,6 +15,9 @@
             <a href="//t.me/yanuaraditia">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-send"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg>
             </a>
+            <a href="//linkedin.com/in/yanuaraditia">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
+            </a>
             <a href="mailto:yanuaraditia@outlook.com">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
             </a>
